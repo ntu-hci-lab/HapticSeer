8ae549ce40074d7014a2b93aea0fb77a8459b96d
Solla <willieyuwei4@gmail.com>
2020-02-17 02:52:54 +0800

    First Commit

---
+++ HapticAgent/HapticAgent/MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// MainPage 類別的宣告。
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace HapticAgent
+{
+	/// <summary>
+	/// 可以在本身使用或巡覽至框架內的空白頁面。
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	};
+}
