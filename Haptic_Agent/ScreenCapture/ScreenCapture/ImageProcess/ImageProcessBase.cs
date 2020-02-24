7d962e594c20b5e49f3f313e392cd0d17df77c91
Solla <willieyuwei4@gmail.com>
2020-02-25 05:00:31 +0800

    First Init

---
+++ Haptic_Agent/ScreenCapture/ScreenCapture/ImageProcess/ImageProcessBase.cs
@@ -0,0 +1,157 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WPFCaptureSample.ScreenCapture.ImageProcess
+{
+    class ImageProcessBase
+    {
+        protected virtual double Clipped_Left
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        protected virtual double Clipped_Top
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        protected virtual double Clipped_Right
+        {
+            get
+            {
+                return 1;
+            }
+        }
+        protected virtual double Clipped_Bottom
+        {
+            get
+            {
+                return 1;
+            }
+        }
+        protected virtual double Scale_Width
+        {
+            get
+            {
+                return 1;
+            }
+        }
+        protected virtual double Scale_Height
+        {
+            get
+            {
+                return 1;
+            }
+        }
+        private bool IsImageClip
+        {
+            get
+            {
+                return !(Clipped_Left == 0 && Clipped_Right == 1 && Clipped_Top == 0 && Clipped_Bottom == 1);
+            }
+        }
+        private bool IsImageScale
+        {
+            get
+            {
+                return !(Scale_Width == 1 && Scale_Height == 1);
+            }
+        }
+        protected volatile bool IsProcessingData = false;
+        private volatile bool IsUpdatingData = false;
+        protected Mat Data = new Mat();
+        private Mat ScaledRawData = new Mat();
+        private static List<ImageProcessBase> imageProcesses = new List<ImageProcessBase>();
+        protected virtual void ImageHandler(object args)
+        {
+
+        }
+        public ImageProcessBase(bool IsAddImageProcessList = true, bool IsCreateNewThread = true)
+        {
+            if (IsAddImageProcessList)
+                imageProcesses.Add(this);
+            if (IsCreateNewThread)
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ImageHandler));
+        }
+        public static void TryUpdateAllData(Mat rawData)
+        {
+            CvInvoke.Imwrite("O:\\TT.png", rawData);
+            foreach (ImageProcessBase imgProc in imageProcesses)
+                imgProc.TryUpdateData(rawData);
+        }
+        public void TryUpdateData(Mat rawData)
+        {
+            if (IsProcessingData)
+                return;
+            else if (IsUpdatingData)
+                return;
+            else
+                IsUpdatingData = true;
+
+            int ImageHeight, ImageWidth;
+            if (!IsImageScale)
+            {
+                ImageHeight = rawData.Rows;
+                ImageWidth = rawData.Cols;
+                ScaledRawData = rawData;
+            }else
+            {
+                ImageHeight = (int)(rawData.Rows * Scale_Height);
+                ImageWidth = (int)(rawData.Cols * Scale_Width);
+                if (ScaledRawData.Rows != ImageHeight || ScaledRawData.Cols != ImageWidth)
+                {
+                    ScaledRawData?.Dispose();
+                    ScaledRawData = new Mat(ImageHeight, ImageWidth, DepthType.Cv8U, 4);
+                }
+                CvInvoke.Resize(rawData, ScaledRawData, ScaledRawData.Size);
+            }
+
+            if (!IsImageClip)
+            {
+                if (Data.Rows != ImageHeight || Data.Cols != ImageWidth)
+                {
+                    Data?.Dispose();
+                    Data = new Mat(ImageHeight, ImageWidth, DepthType.Cv8U, 4);
+                }
+                Utilities.CopyMemory(Data.DataPointer, ScaledRawData.DataPointer, ImageHeight * ImageWidth * 4);
+            }else
+            {
+                int ImageLeft = (int)(ImageWidth * Clipped_Left),
+                    ImageRight = (int)(ImageWidth * Clipped_Right),
+                    ImageTop = (int)(ImageHeight * Clipped_Top),
+                    ImageBottom = (int)(ImageHeight * Clipped_Bottom);
+                int Data_Width = ImageRight - ImageLeft,
+                    Data_Height = ImageBottom - ImageTop;
+                if (Data.Rows != Data_Height || Data.Cols != Data_Width)
+                {
+                    Data?.Dispose();
+                    Data = new Mat(Data_Height, Data_Width, DepthType.Cv8U, 4);
+                }
+                int RawDataOffset = 
+                    + 4 * ImageTop * ImageWidth  //Skip Top Pixel
+                    + 4 * ImageLeft;    //Skip Left Pixel
+                int Length = 4 * Data_Width;
+                for (int i = 0; i < Data_Height; ++i)
+                {
+                    int OutputDataOffset = (4 * i * Data_Width);    //4 Bytes * Data_Width * ith rows
+                    Utilities.CopyMemory(Data.DataPointer + OutputDataOffset, ScaledRawData.DataPointer + RawDataOffset, Length);
+                    RawDataOffset += 4 * ImageWidth;
+                }
+            }
+
+            IsUpdatingData = false;
+            IsProcessingData = true;
+        }
+    }
+}
---
+++ Haptic_Agent/ScreenCapture/ScreenCapture/ImageProcess/OpticalFlow.cs
@@ -0,0 +1,82 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Drawing;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+namespace WPFCaptureSample.ScreenCapture.ImageProcess
+{
+    class OpticalFlow : ImageProcessBase
+    {
+        protected override double Scale_Width
+        {
+            get
+            {
+                return 0.5f;
+            }
+        }
+        protected override double Scale_Height
+        {
+            get
+            {
+                return 0.5f;
+            }
+        }
+        private bool IsStopRunning = false;
+        private Mat LastImg = null;
+        ~OpticalFlow()
+        {
+            IsStopRunning = true;
+        }
+
+        private void featureTracking(Mat img_1, Mat img_2, ref PointF[] points1, out PointF[] points2, out byte[] status)
+        {
+            //this function automatically gets rid of points for which tracking fails
+            float[] err;
+            Size winSize = new Size(21, 21);
+            MCvTermCriteria termcrit = new MCvTermCriteria(30, 0.01);
+            CvInvoke.CalcOpticalFlowPyrLK(img_1, img_2, points1, winSize, 3, new MCvTermCriteria(20, 0.03), out points2, out status, out err);
+            //getting rid of points for which the KLT tracking failed or those who have gone outside the frame
+            int indexCorrection = 0;
+            for (int i = 0; i < status.Length; i++)
+            {
+                PointF pt = points2.ElementAt(i - indexCorrection);
+                if ((status.ElementAt(i) == 0) || (pt.X < 0) || (pt.Y < 0))
+                {
+                    if ((pt.X < 0) || (pt.Y < 0))
+                    {
+                        status.SetValue(0, i);
+                    }
+                    points1.At(points1.begin() + (i - indexCorrection));
+                    points2.erase(points2.begin() + (i - indexCorrection));
+                    indexCorrection++;
+                }
+
+            }
+
+        }
+        protected override void ImageHandler(object args)
+        {
+            while (!IsStopRunning)
+            {
+                while (!IsProcessingData)
+                    Thread.Sleep(1);
+                if (LastImg == null)
+                {
+                    LastImg = new Mat(Data.Size, Data.Depth, Data.NumberOfChannels);
+                    goto EndOfProcess;
+                }
+                CvInvoke.Imwrite("O:\\Test.png", Data);
+                return;
+                EndOfProcess:
+                IsProcessingData = false;
+            }
+        }
+
+    }
+}
