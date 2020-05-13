33bf98f91e49165dfa148c71a18de9d91ffe852e
Solla <willieyuwei4@gmail.com>
2020-05-14 02:01:29 +0800

    Add receive function temp

--- DirectShowTest/DirectShowTest/DirectShowTest/Form1.cs
+++ DirectShowTest/DirectShowTest/DirectShowTest/Form1.cs
@@ -15,6 +15,7 @@ namespace DirectShowTest
     public partial class Form1 : Form
     {
         FilterInfoCollection videoDevices;
+        VideoCaptureDevice device;
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +24,13 @@ namespace DirectShowTest
         private void Form1_Load(object sender, EventArgs e)
         {
             videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            var a = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            device = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            device.NewFrame += Device_NewFrame;
+        }
+
+        private void Device_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            //eventArgs.Frame.Save(@"D:\File.png");
         }
     }
 }
