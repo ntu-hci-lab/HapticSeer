982f811a1c100289d5acb072b95a0f1e7fe5ab90
Solla <willieyuwei4@gmail.com>
2020-05-14 01:50:01 +0800

    First Implement

---
+++ DirectShowTest/DirectShowTest/DirectShowTest/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using System.Threading;
+
+namespace DirectShowTest
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection videoDevices;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            var a = new VideoCaptureDevice(videoDevices[0].MonikerString);
+        }
+    }
+}
