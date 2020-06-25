a8ab1d4eaca70208a3751367465368b951a6c3da
Solla <willieyuwei4@gmail.com>
2020-06-26 04:08:09 +0800

    Code Beautify

--- TestPS4Latency/TestPS4Latency/Program.cs
+++ TestPS4Latency/TestPS4Latency/Program.cs
@@ -4,62 +4,38 @@ using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using Emgu.CV;
-using Emgu.CV.Structure;
+using System.Drawing;
 using AForge.Video.DirectShow;
-using System.Drawing.Bitmap;
 namespace TestPS4Latency
 {
     class Program
     {
         [DllImport("kernel32")]
         extern static UInt64 GetTickCount64();
-        static Mat frame = new Mat();
-        static VideoCapture cap = new VideoCapture();
         static ulong StartRecordTimeStamp = 0;
-        static FilterInfoCollection USB_Webcams = null;
-        static VideoCaptureDevice Cam = null;
 
-        static void ProcessFrame(object sender, EventArgs e)
+        static FilterInfoCollection videoDevices;
+        static VideoCaptureDevice device;
+        static int Counter = 0;
+
+        private static void Device_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
         {
-            if (cap != null && cap.Ptr != IntPtr.Zero)
-            {
-                cap.Retrieve(frame, 0);
-                if (StartRecordTimeStamp > 0)
-                {
-                    ulong Timestamp = GetTickCount64();
-                    ulong delta = Timestamp - StartRecordTimeStamp;
-                    frame.Save(delta.ToString() + ".png");
-                }
+            Counter = ++Counter % 64;
 
+            if (Counter == 0)
+            {
+                ulong Timestamp = GetTickCount64();
+                ulong delta = Timestamp - StartRecordTimeStamp;
+                string fileName = $"D:\\TempOut\\{delta}.png";
+                eventArgs.Frame.Save(fileName);
             }
         }
-        static void ForCaptureCard()
-        {
-            USB_Webcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            if (USB_Webcams.Count > 0)  // The quantity of WebCam must be more than 0.
-            {
-                Cam = new VideoCaptureDevice(USB_Webcams[0].MonikerString);
-                Cam.NewFrame += Cam_NewFrame;
-                Cam.Start();
-                Thread.Sleep(100);
-            }
-
-        }
-
-        private static void Cam_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
-        {
-            if (StartRecordTimeStamp == 0)
-                return;
-            ulong Timedelta = GetTickCount64() - StartRecordTimeStamp;
-            eventArgs.Frame.Save(Timedelta.ToString() + ".png");
-        }
-
         static void Test()
         {
+            string WebUrl = "http://localhost:2222/";// "http://192.168.50.242:2222/";
             HttpListener server = new HttpListener();
-            //server.Prefixes.Add("http://192.168.1.103:2222/");
-            server.Prefixes.Add("http://localhost:2222/");
+            server.Prefixes.Add(WebUrl);
+            //server.Prefixes.Add("http://localhost:2222/");
 
             server.Start();
 
@@ -83,7 +59,7 @@ namespace TestPS4Latency
                         break;
                     default:
                         Console.WriteLine(context.Request.QueryString.Get("test"));
-                        builder = new StringBuilder("<!DOCTYPE html><html><head><script>function A(){fetch('http://localhost:2222/timestamp').then(function(response) {return response.text()}).then(function(html) {document.body.innerText = html;}).catch(function(err) {console.log('Failed to fetch page: ', err);});}setInterval(\"A()\", 1);</script></head><body></body></html>");
+                        builder = new StringBuilder($"<!DOCTYPE html><html><head><script>function A(){{fetch('{WebUrl}timestamp').then(function(response) {{return response.text()}}).then(function(html) {{document.body.innerText = html;}}).catch(function(err) {{console.log('Failed to fetch page: ', err);}});}}setInterval(\"A()\", 1);</script></head><body></body></html>");
                         break;
                 }
 
@@ -95,20 +71,22 @@ namespace TestPS4Latency
 
                 context.Response.Close();
             }
-
         }
         static void Main(string[] args)
         {
-            cap.ImageGrabbed += new EventHandler(ProcessFrame);
-            cap.Start();
-            Thread.Sleep(100);
+
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            device = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+
             StartRecordTimeStamp = GetTickCount64();
             new Thread(() =>
             {
                 Test();
             }).Start();
             while (true)
-                Thread.Sleep(1); ;
+                Thread.Sleep(100);
         }
     }
 }
