a8f56959679def2b18ad246d162c47ded5a85a87
RavenCheng1120 <ravencheng66@gmail.com>
2020-07-17 11:44:47 +0800

    added Speed detection to Screen Capture

--- ScreenCapture/ScreenCapture/Program.cs
+++ ScreenCapture/ScreenCapture/Program.cs
@@ -1,16 +1,19 @@
 ﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 using ImageProcessModule;
 using ImageProcessModule.ProcessingClass;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Management;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Tesseract;
 
 namespace ScreenCapture
 {
@@ -18,6 +21,12 @@ namespace ScreenCapture
     {
         static BitmapBuffer bitmapBuffer = new BitmapBuffer();
         static CaptureMethod captureMethod;
+        /// Initialize Tesseract object
+        /// Remember to add tessdata directory
+        static TesseractEngine ocr = new TesseractEngine("./tessdata", "eng", EngineMode.Default);
+        static KalmanFilter filter = new KalmanFilter(1, 1, 0.05, 1, 0.1, speed);
+        static int speed = 0; // current speed
+        static int preSpeed = 0; // previous speed
         /// <summary>
         /// Parse Argument to get the Capture Method
         /// </summary>
@@ -69,9 +78,76 @@ namespace ScreenCapture
 
             //BarBloodIndicatorDetector barBloodIndicatorDetector = new BarBloodIndicatorDetector();
 
+            // Speed detection
+            ImageProcess SpeedDetection = new ImageProcess(0, 1, 0, 1, ImageProcessBase.ImageScaleType.OriginalSize);
+            SpeedDetection.NewFrameArrivedEvent += SpeedDetection_NewFrameArrivedEvent;
+
             // Do Cache Optimizer
             CacheOptimizer.Init();
             CacheOptimizer.ResetAllAffinity();
+        }
+
+        private static void SpeedDetection_NewFrameArrivedEvent(ImageProcess sender, Mat mat)
+        {
+            /* declare variables for Tesseract */
+            string speedStr;
+            Pix pixImage;
+            Page page;
+            double leftPosition = (1541d / 1720) * mat.Cols;
+            double topPosition = (865d / 1080) * mat.Rows;
+            double widthBound = (60d / 1720) * mat.Cols;
+            double heightBound = (38d / 1080) * mat.Rows;
+
+            /* declare variables*/
+            Rectangle cropArea = new Rectangle((int)leftPosition, (int)topPosition, (int)widthBound, (int)heightBound);
+            SpeedImageProcess speedImageProcess = new SpeedImageProcess();
+            
+
+            try
+            {
+                Bitmap BitmapFrame = Crop_frame(mat, cropArea).ToBitmap();
+                /* image processing */
+                // BitmapFrame = speedImageProcess.NegativePicture(BitmapFrame); //turn into negative image
+                BitmapFrame = speedImageProcess.ResizeImage(BitmapFrame, 120, 76); // enlarge image(x2)
+
+
+                pixImage = PixConverter.ToPix(BitmapFrame); // PixConverter is unable to work at Tesseract 3.3.0
+                page = ocr.Process(pixImage);
+                speedStr = page.GetText(); // Recognized result
+                page.Dispose();
+
+                /* Parse str to int */
+                bool isParsable = Int32.TryParse(speedStr, out speed);
+                if (!isParsable)
+                {
+                    // Console.WriteLine("Speed could not be parsed.");
+                    speed = preSpeed; // Can't detect speed, use the previous speed value
+                }
+
+                /* Prevent negative number or large number */
+                if (speed < 0 || speed > 300)
+                    speed = preSpeed;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error message: " + ex.Message);
+            }
+            // Console.WriteLine("  -Current speed: " + speed + " mph");
+            preSpeed = speed;
+
+            /* Filtering(denoise) */
+            speed = (int)filter.Output(speed);
+            Console.WriteLine("  -Smoothed speed: " + speed + " mph\n");
+        }
+
+        static Image<Bgr, Byte> Crop_frame(Mat input, Rectangle crop_region)
+        {
+            Image<Bgr, Byte> buffer_im = input.ToImage<Bgr, Byte>();
+            buffer_im.ROI = crop_region;
+            Image<Bgr, Byte> cropped_im = buffer_im.Copy();
+            buffer_im.Dispose();
+
+            return cropped_im;
         }
 
         private static void DamageIndicatorDetection_NewFrameArrivedEvent(ImageProcess sender, Mat mat)
