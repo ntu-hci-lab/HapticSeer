cf1bc12cc2ed33fe0754078baa664519b56aa1e2
wendee <b05705046@ntu.edu.tw>
2020-07-21 23:52:30 +0800

    Finish bullet number detection

--- ScreenCapture/ScreenCapture/Program.cs
+++ ScreenCapture/ScreenCapture/Program.cs
@@ -31,7 +31,7 @@ namespace ScreenCapture
             Project_Cars,
             BF1
         }
-        static GameType RunningGameType = GameType.HL_A;
+        static GameType RunningGameType = GameType.BF1;
         static BitmapBuffer bitmapBuffer = new BitmapBuffer();
         static CaptureMethod captureMethod;
         /// Initialize Tesseract object
@@ -117,14 +117,14 @@ namespace ScreenCapture
                     ImageProcess BloodDetector_BF1 = new ImageProcess(1500 / 1728f, 1700 / 1728f, 1028 / 1080f, 1029 / 1080f, ImageScaleType.OriginalSize, FrameRate: 15);
                     BloodDetector_BF1.NewFrameArrivedEvent += BloodDetector_BF1_NewFrameArrivedEvent;
                     //ImageProcess BulletCount_BF1 = new ImageProcess(1526 / 1728f, 1594 / 1728f, 948 / 1080f, 988 / 1080f, ImageScaleType.OriginalSize, FrameRate: 3);
-                    ImageProcess BulletCount_BF1 = new ImageProcess(0.89, 0.922, 0.865, 0.93, ImageScaleType.OriginalSize, FrameRate: 3); // 1920*1080
+                    ImageProcess BulletCount_BF1 = new ImageProcess(0.89, 0.922, 0.865, 0.93, ImageScaleType.OriginalSize, FrameRate: 1); // 1920*1080
                     BulletCount_BF1.NewFrameArrivedEvent += BulletCount_BF1_NewFrameArrivedEvent;
-                    ImageProcess GrenadeCount_BF1 = new ImageProcess(1598 / 1728f, 1628 / 1728f, 978 / 1080f, 998 / 1080f, ImageScaleType.OriginalSize, FrameRate: 3);
-                    GrenadeCount_BF1.NewFrameArrivedEvent += GrenadeCount_BF1_NewFrameArrivedEvent;
+                    //ImageProcess GrenadeCount_BF1 = new ImageProcess(1598 / 1728f, 1628 / 1728f, 978 / 1080f, 998 / 1080f, ImageScaleType.OriginalSize, FrameRate: 3);
+                    //GrenadeCount_BF1.NewFrameArrivedEvent += GrenadeCount_BF1_NewFrameArrivedEvent;
                     ImageProcesses.Add(DamageIndicatorDetection);
                     ImageProcesses.Add(BloodDetector_BF1);
                     ImageProcesses.Add(BulletCount_BF1);
-                    ImageProcesses.Add(GrenadeCount_BF1);
+                    //ImageProcesses.Add(GrenadeCount_BF1);
                     break;
             }
             // Do Cache Optimizer
@@ -132,13 +132,13 @@ namespace ScreenCapture
             CacheOptimizer.ResetAllAffinity();
         }
 
+        /* No need for now */
         private static void GrenadeCount_BF1_NewFrameArrivedEvent(ImageProcess sender, Mat mat)
         {
             if (!sender.Variable.ContainsKey("BinaryImage"))
                 sender.Variable.Add("BinaryImage", new Mat(mat.Size, DepthType.Cv8U, 1));
             Mat BinaryImg = sender.Variable["BinaryImage"] as Mat;
             ImageProcess.ElimateBackgroundWithSearchingSimilarColor(in mat, ref BinaryImg, new Color[] { Color.FromArgb(220, 220, 220) }, new uint[] { 0x00FF0000 }, ElimateColorApproach.ReserveSimilarColor_RemoveDifferentColor, 50);
-            // TODO OCR to BinaryImg
         }
 
         private static void BulletCount_BF1_NewFrameArrivedEvent(ImageProcess sender, Mat mat)
@@ -155,15 +155,15 @@ namespace ScreenCapture
             try
             {
                 pixImage = PixConverter.ToPix(BinaryImg.ToBitmap());
+                ocr.DefaultPageSegMode = PageSegMode.SingleBlock;
                 page = ocr.Process(pixImage);
                 var bulletStr = page.GetText(); // 識別後的內容
 
                 if (!string.IsNullOrEmpty(bulletStr))
                 {
-                    Console.WriteLine("String result : " + bulletStr);
                     Save(BinaryImg, DateTime.Now.Ticks.ToString());
-                    
-                    if(Int32.TryParse(bulletStr, out int num))
+
+                    if (Int32.TryParse(bulletStr, out int num))
                     {
                         Console.WriteLine("bullet number :"+ num.ToString());
                     }
