b402709984fb31230d5b344da1837eb71410edee
eKL016 <yuhsin.lin@outlook.com>
2020-07-27 16:53:02 +0800

    Fix firing detector

--- EventExtractor/StateObject.cs
+++ EventExtractor/StateObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using RedisEndpoint;
 
 namespace EventDetectors
 {
@@ -8,8 +9,12 @@ namespace EventDetectors
     {
         private byte triggerState = 0;
         private ushort bulletCount = 0;
+        private DateTime lastTriggerExit = DateTime.Now;
+        public Publisher publisher;
 
+        public StateObject(Publisher publisher) => this.publisher = publisher;
         public byte TriggerState { get => triggerState; set => triggerState = value; }
         public ushort BulletCount { get => bulletCount; set => bulletCount = value; }
+        public DateTime LastTriggerExit { get => lastTriggerExit; set => lastTriggerExit = value; }
     }
 }
