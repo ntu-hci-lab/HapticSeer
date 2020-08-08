d61f57263c70822412937532ee83aa7fccc68df9
eKL016 <yuhsin.lin@outlook.com>
2020-08-08 16:43:35 +0800

    First version of firing logic

--- EventExtractor/WeaponState.cs
+++ EventExtractor/WeaponState.cs
@@ -7,19 +7,12 @@ namespace EventDetectors
 {
     public class WeaponState
     {
-        private double triggerState = 0;
+        private bool triggerState = false;
         private ushort bulletCount = 0;
-        private bool isAutoFire = false;
-        private DateTime lastTriggerEnter = DateTime.Now;
-        private DateTime lastTriggerExit = DateTime.Now;
         
         public Publisher publisher;
-        public double TriggerState { get => triggerState; set => triggerState = value; }
+        public bool TriggerState { get => triggerState; set => triggerState = value; }
         public ushort BulletCount { get => bulletCount; set => bulletCount = value; }
-        public bool IsAutoFire { get => isAutoFire; set => isAutoFire = value; }
-        public DateTime LastTriggerEnter { get => lastTriggerEnter; set => lastTriggerEnter = value; }
-        public DateTime LastTriggerExit { get => lastTriggerExit; set => lastTriggerExit = value; }
-        
 
         public WeaponState(Publisher publisher) => this.publisher = publisher;
 
