95989b0ca65d6e03a9490626fe44d67dc3a1508a
eKL016 <yuhsin.lin@outlook.com>
2020-08-22 22:18:52 +0800

    Add dashboard and rename Inertia Detector

--- InertiaDetector/InertiaFunctions.cs
+++ InertiaDetector/InertiaFunctions.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 namespace EventDetectors
 {
     public static class InertiaFunctions
     {
         const double HANDLER_MAX_ANGLE = 15d;
 
+# if LOG
+        public static DateTime startTime = DateTime.Now;
+        public static StreamWriter csvWriter = new StreamWriter(startTime.ToString("mm_ss_fff") + "_predict.csv")
+        {
+            AutoFlush = true
+        };
+# endif
         public static void Router(string channelName, string msg, ref StateObject state)
         {
             var start = Program.globalSW.ElapsedTicks;
@@ -12,7 +21,7 @@ namespace EventDetectors
             {
                 case "XINPUT":
                     int headerPos = msg.IndexOf('|');
-                    if(msg.Substring(0, headerPos)== "ThumbLX")
+                    if (msg.Substring(0, headerPos)== "ThumbLX")
                         UpdateNormalState(msg.Substring(headerPos+1), ref state);
                     break;
                 case "SPEED":
@@ -25,17 +34,23 @@ namespace EventDetectors
         }
         public static void UpdateSpeedState(string msg, ref StateObject state)
         {
+            string[] splited = msg.Split(',');
             try
             {
-                ushort.TryParse(msg, out ushort parsedSpeed);
+                ushort.TryParse(splited[0], out ushort parsedSpeed);
                 if (parsedSpeed == 0) return;
                 state.Speed = parsedSpeed;
                 state.Angle = state.LastAngle;
-                
-                Console.WriteLine($"Speed: {parsedSpeed}");
+                if (state.accXOutlet != null) state.publisher.Publish(state.accXOutlet, $"{state.AccelX.ToString()}");
+                if (state.accYOutlet != null) state.publisher.Publish(state.accYOutlet, $"{state.AccelY.ToString()}");
+# if LOG
+                csvWriter.WriteLine($"{(DateTime.Now - startTime).Ticks / TimeSpan.TicksPerMillisecond},{state.AccelX},{state.AccelY}");
+# endif
+                Console.WriteLine(state.AccelX);
             }
             catch (Exception e) 
             {
+                if (e is IndexOutOfRangeException) throw e;
                 Console.WriteLine(e.Message);
             }
         }
@@ -46,8 +61,10 @@ namespace EventDetectors
             {
                 short.TryParse( msg.Split('|')[1], out short parsedHandler );
                 state.LastAngle = (double) parsedHandler / (double) short.MaxValue * HANDLER_MAX_ANGLE;
-            } catch (Exception e)
+            } 
+            catch (Exception e)
             {
+                if (e is IndexOutOfRangeException) throw e;
                 Console.WriteLine(e.Message);
             }
         }
--- ScreenCapture/ScreenCapture/FeatureExtractors.cs
+++ ScreenCapture/ScreenCapture/FeatureExtractors.cs
@@ -12,19 +12,19 @@ namespace ScreenCapture
 
         /// Initialize Tesseract object
         /// Remember to add tessdata directory
-        protected static TesseractEngine ocr = new TesseractEngine(Path.GetFullPath(@"..\..\"), "KomuB", EngineMode.Default);
+        protected static TesseractEngine ocr = new TesseractEngine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory), "KomuB", EngineMode.Default);
         protected static Publisher publisher = new Publisher("localhost", 6380);
         protected List<ImageProcess> ImageProcessesList = new List<ImageProcess>();
         protected FeatureExtractors() {}
 
-        public static FeatureExtractors InitFeatureExtractor(int gameID)
+        public static FeatureExtractors InitFeatureExtractor(int gameID, string[] outlets)
         {
             switch (gameID)
             {
                 case 1:
                     return new HLA();
                 case 2:
-                    return new PC2();
+                    return new PC2(outlets[0]);
                 case 3:
                     return new BF1();
                 default:
