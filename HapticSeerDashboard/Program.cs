1f729169db9a0b9235050c1cf611e76bfb45a4e4
eKL016 <yuhsin.lin@outlook.com>
2020-08-26 14:37:39 +0800

    Refactor

--- HapticSeerDashboard/Program.cs
+++ HapticSeerDashboard/Program.cs
@@ -3,13 +3,13 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using static HapticSeerDashboard.Schema;
 namespace HapticSeerDashboard
 {
     class Program
     {
         public readonly static string SolutionRoot = Path.GetFullPath(Path.Combine(
             AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\.."));
-        public static List<NodeBaseInstance> nodes = new List<NodeBaseInstance>();
         public static Dictionary<string, string> paths =
             JsonConvert.DeserializeObject<Dictionary<string, string>>
             (
@@ -22,40 +22,38 @@ namespace HapticSeerDashboard
                     )
                 )
             );
+
         static void Main(string[] args)
         {
-            using (StreamReader f = File.OpenText(Path.Combine(
-                AppDomain.CurrentDomain.BaseDirectory,
-                "sampleConfig3.json")))
+            if(args.Length == 0)
             {
-                Console.WriteLine($"Solution Root is: {SolutionRoot}");
-                JsonSerializer serializer = new JsonSerializer();
-                ConfigSchema schema = (ConfigSchema)serializer.Deserialize(f, typeof(ConfigSchema));
-
-                foreach (var detector in schema.eventDetectors)
-                {
-                    nodes.Add(new NodeBaseInstance(Path.Combine(SolutionRoot,
-                        paths[detector.Type]), detector.Inlets, detector.Outlets, detector.Options));
-                }
-                foreach (var extractor in schema.extractorSets)
-                {
-                    nodes.Add(new ExtractorSet(Path.Combine(SolutionRoot,
-                        paths["FeatureExtract"]), extractor.Type, extractor.Outlets, extractor.Options));
-                }
-                foreach (var capturer in schema.rawCapturers)
-                {
-                    nodes.Add(new RawCapturer(Path.Combine(SolutionRoot,
-                        paths[capturer.Type]), capturer.Outlets, capturer.Options));
-                }
-
-                foreach (var node in nodes)
-                {
-                    Console.WriteLine(node.ExecutablePath);
-                    node.Launch();
-                }
-                _ = Console.ReadKey();
+                throw new MissingFieldException("No Configuration File Provided!");
             }
-            
+            using StreamReader f = File.OpenText(Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                args[0]));
+            Console.WriteLine($"Solution Root is: {SolutionRoot}");
+            JsonSerializer serializer = new JsonSerializer();
+            Components.LoadPath(paths, SolutionRoot);
+            Schema schema = (Schema)serializer.Deserialize(f, typeof(Schema));
+
+            foreach (var cap in schema.rawCapturers)
+            {
+                Console.WriteLine(cap.ExecutablePath);
+                cap.Launch();
+            }
+            foreach (var ext in schema.extractorSets)
+            {
+                Console.WriteLine(ext.ExecutablePath);
+                ext.Launch();
+            }
+            foreach (var dect in schema.eventDetectors)
+            {
+                Console.WriteLine(dect.ExecutablePath);
+                dect.Launch();
+            }
+            _ = Console.ReadKey();
+
         }
     }
 }
---
+++ HapticSeerDashboard/Schema.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace HapticSeerDashboard
+{
+    public class Schema
+    {
+        abstract public class Components
+        {
+            private static string solutionRoot;
+            private string name, preset;
+            private string[] inlets, outlets;
+            private bool enableOutput = false;
+            private Process process;
+            protected static Dictionary<string, string> paths;
+            [JsonExtensionData]
+            protected IDictionary<string, JToken> _additionalData;
+
+
+            public string ExecutablePath { get => GetPath(Name, Preset); set { } }
+            public string Name { get => name; set => name = value; }
+            public string Preset { get => preset; set => preset = value; }
+            public string[] Inlets { get => inlets; set => inlets = value; }
+            public string[] Outlets { get => outlets; set => outlets = value; }
+            public bool EnableOutput { get => enableOutput; set => enableOutput = value; }
+            public Dictionary<string, string> Options { get; set; }
+            public string SolutionRoot { get => solutionRoot; set => solutionRoot = value; }
+
+            public Components()
+            {
+                _additionalData = new Dictionary<string, JToken>();
+            }
+
+
+            virtual protected string GetPath(string type, string subtype)
+            {
+                if (paths == null) throw new InvalidOperationException("You must load paths first!");
+                paths.TryGetValue($"{type}_{subtype}", out string exePath);
+                return exePath;
+            }
+            static public void LoadPath(Dictionary<string, string> paths, String solutionRoot)
+            {
+                if (Components.paths == null) 
+                {
+                    Components.paths = paths;
+                    Components.solutionRoot = solutionRoot;
+                } 
+            }
+            virtual public void Launch()
+            {
+                if (ExecutablePath == null) throw new MissingFieldException(message: "Executable path not assigned!");
+                process = new Process();
+                process.StartInfo.FileName = Path.Combine(
+                    solutionRoot,
+                    ExecutablePath
+                    );
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                if (inlets != null)
+                {
+                    for (int i = 0; i < inlets.Length; i++) process.StartInfo.ArgumentList.Add(inlets[i]);
+                }
+                if (outlets != null)
+                {
+                    for (int i = 0; i < outlets.Length; i++) process.StartInfo.ArgumentList.Add(outlets[i]);
+                }
+                if (Options != null)
+                {
+                    process.StartInfo.UseShellExecute = Convert.ToBoolean(
+                        Options.GetValueOrDefault("UseShellExecute", "False"));
+                    process.StartInfo.RedirectStandardOutput = Convert.ToBoolean(
+                        Options.GetValueOrDefault("RedirectStandardOutput", "False"));
+                }
+                process.Start();
+            }
+        }
+
+        public class ExtractorSet : Components
+        {
+            public ExtractorSet()
+            {
+
+            }
+            [OnDeserialized]
+            private void OnDeserialized(StreamingContext context)
+            {
+                Inlets = new string[] { Preset };
+            }
+            protected override string GetPath(string type, string subtype)
+            {
+                if (paths == null) throw new InvalidOperationException("You must load paths first!");
+                paths.TryGetValue($"{type}", out string exePath);
+                return exePath;
+            }
+        }
+
+        public class RawCapturer : Components 
+        {
+            public RawCapturer() { }
+            protected override string GetPath(string type, string subtype)
+            {
+                if (paths == null) throw new InvalidOperationException("You must load paths first!");
+                paths.TryGetValue($"{type}", out string exePath);
+                return exePath;
+            }
+        }
+        
+        public class EventDetector : Components
+        {
+            public EventDetector() { }
+        }
+        public List<ExtractorSet> extractorSets = new List<ExtractorSet>();
+        public List<RawCapturer> rawCapturers = new List<RawCapturer>();
+        public List<EventDetector> eventDetectors = new List<EventDetector>();
+    }
+}
